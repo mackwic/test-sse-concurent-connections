a7903478952a4f2267591fd35889f64abce5f194
Thomas Wickham <mackwic@gmail.com>
2021-05-10 11:43:10 +0200

    Fix package.json

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-app.use(express.static('public'))
+const app = express();
+app.use(express.static("public"));
 
-app.get('/countdown', function(req, res) {
+app.get("/countdown", function (req, res) {
   res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  })
-  countdown(res, 120)
-})
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+  countdown(res, 120);
+});
 
 function countdown(res, count) {
-  res.write("data: " + count + "\n\n")
-  if (count)
-    setTimeout(() => countdown(res, count-1), 1000)
-  else
-    res.end()
+  res.write("data: " + count + "\n\n");
+  if (count > 0) setTimeout(() => countdown(res, count - 1), 1000);
+  else res.end();
 }
 
-app.listen(3000, () => console.log('SSE app listening on port 3000!'))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("SSE app listening on port 3000!"));
