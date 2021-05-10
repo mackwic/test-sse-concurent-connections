39824d4d1f090dfd5a660f3423747c4bb2d22b44
Thomas Wickham <mackwic@gmail.com>
2021-05-10 15:20:57 +0200

    display connection_counter

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
+let connection_counter = 0;
+const COUNTER_BASE = 500;
+
 app.get("/countdown", function (req, res) {
+  connection_counter += 1;
   res.writeHead(200, {
     "Content-Type": "text/event-stream",
     "Cache-Control": "no-cache",
     Connection: "keep-alive",
   });
-  countdown(res, 120);
+  countdown(res, COUNTER_BASE);
 });
 
 function countdown(res, count) {
-  res.write("data: " + count + "\n\n");
+  res.write(`conn: ${connection_counter} | data: ${count}\n\n`);
   if (count > 0) setTimeout(() => countdown(res, count - 1), 1000);
-  else res.end();
+  else {
+    connection_counter -= 1;
+    res.end();
+  }
 }
 
 const PORT = process.env.PORT || 3000;
